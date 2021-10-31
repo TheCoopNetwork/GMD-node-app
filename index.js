4ae71e495478b2d530d7e6cc2878f6540032a323
George Asiminei <gasiminei@luxoft.com>
2021-10-31 19:47:33 +0200

    Create installer. Confirmation on exit. Close backend when frontend is closed

--- index.js
+++ index.js
@@ -1,51 +1,110 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog, Tray, Menu } = require('electron');
+if (require('electron-squirrel-startup')) return app.quit(); //do not restart multiple times during installer
+app.quitting=false;
 const path = require('path');
-const { spawn } = require('child_process');
-var bat = require.resolve(path.join(__dirname, 'gmd-node', 'start.bat'));
+const { spawn, exec } = require('child_process');
 const log = require('electron-log');
 var my_url = 'http://localhost:6876';
+let tray = null;
+
+const killBackend = () => {
+	exec("taskkill /f /im gmd-jvm.exe");
+};
+
+var winUtil = {
+	bat: require.resolve(path.join(__dirname, 'gmd-node', 'start.bat'))
+};
+
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+  app.quit()
+}
 
 const createWindow = () => {
-  // Create the browser window.
-  //child_process.exec(path.join(__dirname, 'backend', 'start.bat'), function(error, stdout, stderr) {
-  //  console.log(stdout);
-  //});
-  log.warn('=-========----=========starting bat from:' + bat);
-  var ls = spawn(bat);
-  
-  ls.stdout.on('data', function (data) {
+
+	log.warn('Starting bat from:' + winUtil.bat);
+	var ls = spawn(winUtil.bat);
+
+
+	ls.stdout.on('data', function (data) {
 	log.verbose('stdout: ' + data);
-  });
+	});
 
-  ls.stderr.on('data', function (data) {
-    log.error('stderr: ' + data);
-  });
+	ls.stderr.on('data', function (data) {
+	log.error('stderr: ' + data);
+	});
 
-  ls.on('exit', function (code) {
-    log.info('child process exited with code ' + code);
-  });
-  
-  const mainWindow = new BrowserWindow({
-    width: 1400,
-    height: 800,
-	title: "GMD-Node" + my_url,
-	icon: __dirname + '/logo.ico',
-	frame: true,
-	autoHideMenuBar: true
-  });
+	ls.on('exit', function (code) {
+	log.info('child process exited with code ' + code);
+	});
 
-  // and load the index.html of the app.
-  // mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  
-  mainWindow.loadURL(my_url);
+	const mainWindow = new BrowserWindow({
+		width: 1400,
+		height: 800,
+		title: "GMD-Node " + my_url,
+		icon: __dirname + '/logo.ico',
+		frame: true,
+		autoHideMenuBar: true
+	});
 
+	mainWindow.on('close', function(e){
+		log.debug('closing window');
+		let options  = {
+			title: "Quit?",
+			type: "question",
+			buttons: ["Yes, I want to quit","Minimize to Tray","Cancel"],
+			message: "Keeping the node running by minimizing it in the tray helps COOP Network to maintain a secure and descentralized environment. \n\n\n Are you sure you want to quit?"
+		}
+		if(app.quitting!=true){
+			let resp = dialog.showMessageBoxSync(this, options);
+			log.debug('Quit dialog response=' + resp);			
+			if(resp==0){
+				log.info('quiting...');	
+				killBackend();
+				app.quitting=true;
+				app.quit();
+			} else {
+				e.preventDefault();
+				if (resp == 1) {
+					mainWindow.hide();
+				}
+			}
+		}
+	});
 
+	mainWindow.on('minimize',function(event){
+		event.preventDefault();
+		mainWindow.hide();
+	});
+
+	// and load the index.html of the app.
+	// mainWindow.loadFile(path.join(__dirname, 'index.html'));
+	tray = new Tray(__dirname + '/logo.ico');
+
+	
+	const contextMenu = Menu.buildFromTemplate([
+		{ label: 'Open', click: function() {
+			mainWindow.show();
+		}},
+		{ label: 'Quit', click: function() {
+			app.isQuiting = true;
+			app.quit();
+		} }
+	]);
+	tray.setContextMenu(contextMenu);
+	tray.setToolTip("Connected to node: "+ my_url)
+	tray.on('click', ()=> {
+		mainWindow.show();
+	}); 
+
+	
+	mainWindow.loadURL(my_url);
 };
 
 // This method will be called when Electron has finished
@@ -53,13 +112,20 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
+
+app.on('before-quit', function(evt) {
+	log.info('before quit.');
+	tray.icon =  null;
+	tray.destroy();
+})
+
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   log.info('window closed')
   if (process.platform !== 'darwin') {
-    app.quit();
+    //app.quit();
   }
 });
 
