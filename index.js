a66801af23e254e221ec70725f7f722fdaa0b96e
George Asiminei <george.asiminei@gmail.com>
2021-11-13 21:00:29 +0200

    Add MacOS support; minor bug fixing

--- index.js
+++ index.js
@@ -1,84 +1,81 @@
-const { app, BrowserWindow, dialog, Tray, Menu } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 if (require('electron-squirrel-startup')) return app.quit(); //do not restart multiple times during installer
 app.quitting=false;
-const path = require('path');
-const { spawn, exec } = require('child_process');
+const quitDialogOpts = {
+	title : 'Quit?',
+	type : 'question',
+	buttons : ['Yes, I want to quit','Minimize to Tray','Cancel'],
+	message : 'Keeping the node running by minimizing it in the tray helps COOP Network to maintain a secure and descentralized environment. \n\n\n Are you sure you want to quit?'
+};
+
 const log = require('electron-log');
-var my_url = 'http://localhost:6876';
+const sysTray = require('./tray');
+const my_url = 'http://localhost:6876';
 let tray = null;
 
-var AutoLaunch = require('auto-launch');
-try {
-	var gmdNodeLauncher = new AutoLaunch({
-		name: 'gmd-node',
-		isHidden: true,
-		path: require.resolve(path.join(__dirname, '..', '..', 'GMD-Node.exe'))
-	});
-	gmdNodeLauncher.enable();
-} catch(e){
-	log.error("Cannot autostart "+ JSON.stringify(e));
-};
-
-const killBackend = () => {
-	exec("taskkill /f /im gmd-jvm.exe");
-};
-
-var winUtil = {
-	bat: require.resolve(path.join(__dirname, 'gmd-node', 'start.bat'))
-};
-
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
 
 const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
   app.quit()
 }
 
-const createWindow = () => {
+let os = require('os');
+let osAbstraction = null;
+if(os.platform() == 'win32'){
+	osAbstraction = require('./win');
+} else if (os.platform() == 'darwin'){
+	osAbstraction = require('./macos');
+} else if (os.platform == 'linux') {
+	//TODO
+	osAbstraction = require('./macos');
+} else {
+	log.error("unrecognized platform "+ os.platform());
+	app.quitting = true;
+	app.quit();
+}
 
-	log.warn('Starting bat from:' + winUtil.bat);
-	var ls = spawn(winUtil.bat);
+osAbstraction.setAutostart();
+osAbstraction.startBackend();
+
+waitUntilBackendStarted = async ()=> {
+    let initFinished=false;	
+    const http = require('http');
+    
+    log.debug("while..........")
+	while (initFinished == false){
+		log.debug("inside while");
+
+		http.get(my_url, function (res) {
+			log.info(my_url + " is accessible; creating browser window")			  
+			initFinished = true;
+		}).on('error', async function(e) {
+			log.debug(my_url + " not yet accessible. waiting to initialize");	  
+		});
+		await new Promise(resolve => setTimeout(resolve, 1000));
+	}
+}
 
 
-	ls.stdout.on('data', function (data) {
-	log.verbose('stdout: ' + data);
-	});
-
-	ls.stderr.on('data', function (data) {
-	log.error('stderr: ' + data);
-	});
-
-	ls.on('exit', function (code) {
-	log.info('child process exited with code ' + code);
-	});
-
+const createWindow = async () => {
+	await waitUntilBackendStarted();
 	const mainWindow = new BrowserWindow({
 		width: 1400,
 		height: 800,
 		title: "GMD-Node " + my_url,
-		icon: __dirname + '/logo.ico',
+		icon: __dirname + osAbstraction.getLogoFileName(),
 		frame: true,
 		autoHideMenuBar: true
 	});
 
-	mainWindow.on('close', function(e){
+
+	mainWindow.on('close', function(e) {
 		log.debug('closing window');
-		let options  = {
-			title: "Quit?",
-			type: "question",
-			buttons: ["Yes, I want to quit","Minimize to Tray","Cancel"],
-			message: "Keeping the node running by minimizing it in the tray helps COOP Network to maintain a secure and descentralized environment. \n\n\n Are you sure you want to quit?"
-		}
 		if(app.quitting!=true){
-			let resp = dialog.showMessageBoxSync(this, options);
+			let resp = dialog.showMessageBoxSync(this, quitDialogOpts);
 			log.debug('Quit dialog response=' + resp);			
 			if(resp==0){
 				log.info('quiting...');	
-				killBackend();
+				osAbstraction.killBackend();
 				app.quitting=true;
 				app.quit();
 			} else {
@@ -94,62 +91,28 @@ const createWindow = () => {
 		event.preventDefault();
 		mainWindow.hide();
 	});
-
-	// and load the index.html of the app.
-	// mainWindow.loadFile(path.join(__dirname, 'index.html'));
-	tray = new Tray(__dirname + '/logo.ico');
-
 	
-	const contextMenu = Menu.buildFromTemplate([
-		{ 
-			label: 'Open', 
-			click: function() {
-				mainWindow.show();
-			}
-		},
-		{ 
-			label: 'Refresh', 
-			click: function() {
-				mainWindow.reload();
-			}
-		},
-		{ 
-			label: 'Quit', 
-			click: function() {
-				log.info('quiting...');	
-				killBackend();
-				app.quitting = true;
-				app.quit();
-			}
-		},
-		// { 
-		// 	label: 'Autostart', 
-		// 	type: 'checkbox', 
-		// 	checked: true,
-		// 	click: function() {}
-		// }
-		
-	]);
-	tray.setContextMenu(contextMenu);
-	tray.setToolTip("Connected to node: "+ my_url)
-	tray.on('click', ()=> {
-		mainWindow.show();
-	}); 
+	sysTray.init(
+		osAbstraction.getLogoFileName(),
+		"Connected to node: "+ my_url,
+		()=>{mainWindow.show()},
+		()=>{mainWindow.reload()},
+		()=>{log.info('quiting...');
+			osAbstraction.killBackend();
+			app.quitting = true;
+			app.quit();
+		}
+	)
 
-	
 	mainWindow.loadURL(my_url);
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
 
 app.on('before-quit', function(evt) {
 	log.info('before quit.');
-	tray.icon =  null;
-	tray.destroy();
+	sysTray.destroy();
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -170,6 +133,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
