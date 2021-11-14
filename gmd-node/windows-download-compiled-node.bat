6877f89f72d1ef72ac56460ac1d1cab63cae21e1
George Asiminei <george.asiminei@gmail.com>
2021-11-14 21:05:39 +0200

    script to download compiled GMD node on Windows

---
+++ gmd-node/windows-download-compiled-node.bat
@@ -0,0 +1,18 @@
+@echo off
+
+wget https://node.thecoopnetwork.io:8443/gmd-node-standalone.zip
+powershell Expand-Archive gmd-node-standalone.zip
+
+cd gmd-node-standalone\GMD\GMD-2*
+xcopy /s . ..\..\..\
+cd ..\..\..
+
+set conffile=.\conf\nxt.properties
+echo %conffile%
+echo nxt.apiServerHost=localhost>%conffile%
+echo nxt.myPlatform=Windows App>>%conffile%
+
+del start.sh
+del start.bat
+del gmd-node-standalone.zip
+rmdir /S /Q .\gmd-node-standalone
